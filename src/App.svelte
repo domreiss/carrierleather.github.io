e609fde9bc2e053b7164e5221025b2888604f29b
Dominik <d.b.woeltche@gmail.com>
2023-10-21 23:06:39 +0200

    Go to plain svelte

---
+++ src/App.svelte
@@ -0,0 +1,34 @@
+<script>
+  import logo from "./assets/logo.png";
+</script>
+
+<main>
+  <div class="container">
+    <div>
+      <a href="https://www.youtube.com/@CarrierLeather">
+        <img src={logo} alt="Logo" class="centered" />
+      </a>
+      <div class="social">
+        <!-- ... -->
+      </div>
+    </div>
+  </div>
+</main>
+
+<style>
+  .container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: 100vh;
+  }
+
+  .social {
+    display: flex;
+    justify-content: center;
+  }
+
+  img {
+    width: 300px;
+  }
+</style>
